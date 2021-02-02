ca6d48b3006bdd7976ae6395e455f15f2aef4b1b
willyantoteguh <willyantoteguh1@gmail.com>
2021-02-02 11:34:38 +0700

    Create Navbar

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -16,7 +16,7 @@
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-yellow-300 leading-7 font-semibold"><a href="/dashboard/food">Food</a></div>
+            <div class="ml-14 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/food">Food</a></div>
         </div>
 
        
@@ -24,19 +24,19 @@
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
             <div class="flex items-center">
-                <div class="ml-4 text-lg text-yellow-300 leading-7 font-semibold"><a href="/dashboard/users">Users</a></div>
+                <div class="ml-14 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/users">Users</a></div>
             </div>
     </div>
 
     <div class="p-6 border-t border-gray-200">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-yellow-300 leading-7 font-semibold"><a href="/dashboard/transactions">Transaction</a></div>
+            <div class="ml-14 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/transactions">Transaction</a></div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="/dashboard/profile">Profile</a></div>
+            <div class="ml-14 text-lg text-gray-600 leading-7 font-semibold"><a href="/user/profile">Profile</a></div>
         </div>
     </div>
 </div>
